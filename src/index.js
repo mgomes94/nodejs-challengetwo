e344de0dbc28af7e8d72aa57a4a22f505cd672d0
Matheus Gomes <mgomes@academiadapele.com.br>
2022-05-09 15:49:04 -0300

    NodeJs Course second challenge completed

--- src/index.js
+++ src/index.js
@@ -10,19 +10,63 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (user) {
+    request.user = user;
+    return next();
+  }
+
+  return response.status(404).json({ error: 'User not found!' });
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro || (!user.pro && user.todos.length < 10)) {
+    return next();
+  }
+
+  return response.status(403).json({ error: 'User don\'t have permission to this! ' });
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if (user) {
+
+    if (!validate(id)) {
+      return response.status(400).json({ error: 'Uuid is not valid!' });
+    }
+
+    const todo = user.todos.find(todo => todo.id === id);
+
+    if (todo) {
+      request.user = user;
+      request.todo = todo;
+      return next();
+    }
+  }
+
+  return response.status(404).json({ error: 'User or todo not found!' });
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (user) {
+    request.user = user;
+    return next();
+  }
+
+  return response.status(404).json({ error: 'User not found!' });
 }
 
 app.post('/users', (request, response) => {
